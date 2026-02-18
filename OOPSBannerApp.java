df48bb31cdaf7e025ec7cca71972d354b2fd90cd
yashshyan <yashshyanys1162@gmail.com>
2026-02-18 20:56:36 +0530

    UC2: print OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,11 @@
 public class OOPSBannerApp{
     public static void main(String[] args) {
-        System.out.println("OOPS");
+		System.out.println(" **   **   **   **  ");
+		System.out.println("*     * *     * *     * *     * ");  
+		System.out.println("*     * *     * *     * *       ");
+		System.out.println("*     * *     * **   **   ");
+		System.out.println("*     * *     * *            *  ");
+		System.out.println("*     * *     * *            *  ");
+		System.out.println(" **   **   *       ***   ");
     }
 }
