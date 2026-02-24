99981e02fe280702abd5848e73213f6c891cbbc1
yashshyan <yashshyanys1162@gmail.com>
2026-02-24 15:04:36 +0530

    UC5: print OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,17 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        String[] lines = new String[7];
-        lines[0] = " *****   *****   *****  ******* ";
-        lines[1] = "*     * *     * *     * *       ";
-        lines[2] = "*     * *     * *     * *       ";
-        lines[3] = "*     * *     * *****   ******  ";
-        lines[4] = "*     * *     * *            *  ";
-        lines[5] = "*     * *     * *            *  ";
-        lines[6] = " *****   *****  *       ******  ";
+
+        String[] lines = {
+            String.join("", " *****   ", "*****   ", "*****   ", "*****  "),
+            String.join("", "*     *  ", "*     *  ", "*     *  ", "*     * "),
+            String.join("", "*     *  ", "*     *  ", "*     *  ", "*       "),
+            String.join("", "*     *  ", "*     *  ", "*****   ", "*****   "),
+            String.join("", "*     *  ", "*     *  ", "*        ", "    *  "),
+            String.join("", "*     *  ", "*     *  ", "*        ", "    *  "),
+            String.join("", " *****   ", "*****   ", "*        ", "*****   ")
+        };
+
         for (String line : lines) {
             System.out.println(line);
         }
